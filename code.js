e36201ccb98060bc57fc3fc06cd288810ab7c50c
jeanne <jeanne.ngassa@gmail.com>
2015-12-11 09:19:27 +0100

    dernier commit

---
+++ code.js
@@ -0,0 +1,13 @@
+//<script type="text/javascript">
+
+function addition()
+{
+   var resultat = document.getElementById('résultat');
+    var nombre_a = document.getElementById('nombre_a').value;
+    var nombre_b = document.getElementById('nombre_b').value;
+    var nombre_c = document.getElementById('nombre_c').value;
+    var nombre_d = document.getElementById('nombre_d').value;
+  resultat.value=parseInt(nombre_a)+parseInt(nombre_b)+parseInt(nombre_c)+parseInt(nombre_d);
+
+}
+//</script>
